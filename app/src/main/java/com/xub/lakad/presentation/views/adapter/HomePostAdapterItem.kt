3d2d1cd5e2d08d25e182f14ab7024259015e99fa
Tenten Ponce <poncetenten10@gmail.com>
2019-07-14 09:48:14 +0800

    Added fork prompt

--- app/src/main/java/com/xub/lakad/presentation/views/adapter/HomePostAdapterItem.kt
+++ app/src/main/java/com/xub/lakad/presentation/views/adapter/HomePostAdapterItem.kt
@@ -1,12 +1,18 @@
 package com.xub.lakad.presentation.views.adapter
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.view.View
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.xub.lakad.R
 import com.xub.lakad.presentation.base.FastAdapterItem
+import com.xub.lakad.presentation.common.utils.DialogFactory
 import com.xub.lakad.presentation.common.utils.NumberUtils
+import com.xub.lakad.presentation.views.profile.ProfileActivity
+import kotlinx.android.synthetic.main.item_home_card.view.iv_fork
 import kotlinx.android.synthetic.main.item_home_card.view.iv_home_heart
 import kotlinx.android.synthetic.main.item_home_card.view.tv_foot_count
 import kotlinx.android.synthetic.main.item_home_card.view.tv_fork_count
@@ -57,9 +63,24 @@ class HomePostAdapterItem(destination: String) :
         holder.view.tv_foot_count.text =
             "${NumberUtils.formatDecimal(Random.nextDouble(1.0, 9.9), 1)}k"
 
-        holder.view.tv_fork_count.setOnClickListener {
+        holder.view.tv_fork_count.setOnClickListener { fork(holder.view.context) }
+        holder.view.iv_fork.setOnClickListener { fork(holder.view.context) }
+    }
 
-        }
+    private fun fork(context: Context) {
+        DialogFactory.createConfirmDialog(context,
+            "Fork Itinerary",
+            "Forking this itinerary will get you a copy and allows you to update it and post your own version.",
+            DialogInterface.OnClickListener { dialog, which ->
+                dialog.dismiss()
+                context.startActivity(
+                    Intent(
+                        context,
+                        ProfileActivity::class.java
+                    )
+                )
+            })
+            .show()
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
