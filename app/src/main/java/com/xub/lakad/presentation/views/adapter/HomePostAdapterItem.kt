b321739d9eafa4e9a109857c680fc849824b37d1
Tenten Ponce <poncetenten10@gmail.com>
2019-07-14 09:09:34 +0800

    WIP fork

--- app/src/main/java/com/xub/lakad/presentation/views/adapter/HomePostAdapterItem.kt
+++ app/src/main/java/com/xub/lakad/presentation/views/adapter/HomePostAdapterItem.kt
@@ -6,7 +6,11 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.xub.lakad.R
 import com.xub.lakad.presentation.base.FastAdapterItem
+import com.xub.lakad.presentation.common.utils.NumberUtils
 import kotlinx.android.synthetic.main.item_home_card.view.iv_home_heart
+import kotlinx.android.synthetic.main.item_home_card.view.tv_foot_count
+import kotlinx.android.synthetic.main.item_home_card.view.tv_fork_count
+import kotlin.random.Random
 
 class HomePostAdapterItem(destination: String) :
     FastAdapterItem<String, HomePostAdapterItem, HomePostAdapterItem.ViewHolder>(destination) {
@@ -49,6 +53,13 @@ class HomePostAdapterItem(destination: String) :
             isHeart = !isHeart
         }
 
+        holder.view.tv_fork_count.text = "${Random.nextInt(0, 50)}"
+        holder.view.tv_foot_count.text =
+            "${NumberUtils.formatDecimal(Random.nextDouble(1.0, 9.9), 1)}k"
+
+        holder.view.tv_fork_count.setOnClickListener {
+
+        }
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
